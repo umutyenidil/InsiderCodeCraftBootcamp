ed8c738d9b3678135172a2baaadc40c42857472e
Umut Yenidil <umut.yenidil9@gmail.com>
2025-03-02 23:17:22 +0300

    gorev ekleme modal olusturuldu

--- Intasks/assets/js/main.js
+++ Intasks/assets/js/main.js
@@ -2,6 +2,8 @@ import {buildTaskItem} from "./components/liTaskItem.js";
 import {formatDate} from "./utils.js";
 
 const ulTaskList = document.querySelector("ul.task-list");
+const btnAddTask = document.getElementById("btnAddTask");
+const divOverlay = document.querySelector("div.overlay");
 
 for (let i = 0; i < 20; i++) {
     ulTaskList.appendChild(buildTaskItem({
@@ -25,4 +27,14 @@ ulTaskList.addEventListener("click", ({target}) => {
     if (target.classList.contains("actions__delete")) {
         ulTaskList.removeChild(target.closest("li"));
     }
+});
+
+btnAddTask.addEventListener("click", () => {
+    if (divOverlay.classList.contains("hidden")) divOverlay.classList.remove("hidden");
+    else divOverlay.classList.add("hidden");
+});
+
+divOverlay.addEventListener("click", ({currentTarget, target}) => {
+    if(currentTarget === target) divOverlay.classList.add("hidden");
+    if(target.closest("button")?.id === "btnBack") divOverlay.classList.add("hidden");
 });
